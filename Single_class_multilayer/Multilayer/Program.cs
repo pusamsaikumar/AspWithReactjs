914e0e2dfad691d778027604df9217f136ecd57b
pusamsaikumar <92392024+pusamsaikumar@users.noreply.github.com>
2023-08-31 10:00:30 -0500

    Added Dependency Injection for multiple class repositories

--- Single_class_multilayer/Multilayer/Program.cs
+++ Single_class_multilayer/Multilayer/Program.cs
@@ -1,7 +1,9 @@
+using Employee.Common.Utilities;
 using Employee.Repositories;
 using Employee.Repositories.Interfaces;
 using Employee.Service;
 using Employee.Service.Interfaces;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +13,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IEmployeeService, EmployeeService >();
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
-var app = builder.Build();
+string[] assemblyNames =
+{
+    "Multilayer",
+    "Employee.Common",
+    "Employee.Repositories",
+    "Employee.Service"
+};
+
+
+
+builder.Services.RegisterClassOnMactchingNames(assemblyNames);
+    //builder.Services.AddScoped<IEmployeeService, EmployeeService >();
+    //builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+
+    var app = builder.Build();
 app.UseCors(x => x .AllowAnyMethod() .AllowAnyHeader()  );
 
 // Configure the HTTP request pipeline.
